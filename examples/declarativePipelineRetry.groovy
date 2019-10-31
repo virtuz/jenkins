03bd385444397ecfc036b8583ade45e12b44fb4d
Aliaksandr Bullo <aliaksandr_bullo@epam.com>
2019-11-01 02:09:46 +0300

    adding more "retry" cases
    During playing with different options, my goal was to find way to retry http request until it returns success.
    In case timeout for retry is reached, I need continue pipeline and use http status of last attempt.
    So, I run into different issues:
    1) Use Groovy Sandbox - if this option is enabled, I need to approve some simple things e.g. method java.util.Date getTime
    Plugin version is script-security:1.66
    2) catchError step with catchInterruptions: true, allow you to continue in case timeout is reached, but status of pipeline will be aborted
    Plugin version is workflow-basic-steps:2.18

--- examples/declarativePipelineRetry.groovy
+++ examples/declarativePipelineRetry.groovy
@@ -1,9 +1,12 @@
 #!/usr/bin/env groovy
 
 /**
- * This is an example of declarative pipeline which will "retry" sh script
+ * This is an example of declarative pipeline which will "retry" sh script / http request
  * Please note that despite the fact of specifyed shebang line, this code can NOT be executed via groovy.
  *
+ * Prerequisites:
+ * HTTP Request plugin should be installed in Jenkins.
+ * 
  * Here some references used for build this example:
  * https://jenkins.io/doc/book/pipeline/syntax/#options
  * https://jenkins.io/doc/pipeline/steps/workflow-basic-steps/#catcherror-catch-error-and-set-build-result-to-failure
@@ -11,34 +14,101 @@
  * https://jenkins.io/doc/pipeline/steps/workflow-basic-steps/#timeout-enforce-time-limit
  * https://jenkins.io/doc/pipeline/steps/workflow-basic-steps/#waituntil-wait-for-condition
  * https://jenkins.io/doc/pipeline/steps/workflow-durable-task-step/#sh-shell-script
+ * https://jenkins.io/doc/pipeline/steps/http_request/
+ * https://jenkins.io/doc/book/managing/script-approval/
+ * https://wiki.jenkins.io/display/JENKINS/Script+Security+Plugin
  */
 
 pipeline {
     agent any
-    options { timeout(time: 1, unit: 'DAYS') }
+    options { 
+        timeout(time: 1, unit: 'DAYS') 
+        timestamps()
+    }
+    parameters { booleanParam(name: 'CATCH_INTERRUPTIONS', defaultValue: false, description: 'Used for catchError step for setting catchInterruptions') }
     stages {
-        stage('retry') {
+        stage('waitUntil custom timeout') {
             steps {
-                catchError(message: 'retry gave up, but we continue...', buildResult: 'SUCCESS', stageResult: 'SUCCESS'){
-                    retry(count: 10) {
-                        sh "exit 1"
+                script {
+                    long timeout = getUnixTime() +  5 * 1000
+                    waitUntil {
+                        isTimeoutReached(timeout)
                     }
                 }
             }
         }
-        stage('timeout + waitUntil') {
-            options { timeout(time: 1, unit: 'HOURS') }
+        stage('retry') {
             steps {
-                timeout(time: 1, unit: 'MINUTES') {
-                    waitUntil {
-                        executeShellScript 'exit 1'       
+                echo "before catchError"
+                catchError(message: 'retry gave up, but we continue...', buildResult: 'SUCCESS', stageResult: 'SUCCESS') {
+                    retry(count: 5) {
+                        echo "inside retry"
+                        sh "exit 1"
                     }
                 }
+                echo "after catchError"
+            }
+        }
+        stage('waitUntilSuccessfulResponse') {
+            options { timeout(time: 1, unit: 'HOURS') }
+            steps {
+                script {
+                    def response = waitUntilSuccessfulResponse 'http://localhost'
+                    echo "final http status code is ${response?.status}"
+                }
+            }
+        }
+        stage('timeout + waitUntil + executeShellScript') {
+            options { timeout(time: 1, unit: 'HOURS') }
+            steps {
+                echo "before catchError, catchInterruptions: ${params.CATCH_INTERRUPTIONS}"
+                catchError(message: 'waitUntil gave up, but we continue...', catchInterruptions: params.CATCH_INTERRUPTIONS, buildResult: 'SUCCESS', stageResult: 'SUCCESS') {
+                    timeout(time: 5, unit: 'SECONDS') {
+                        waitUntil {
+                            echo "inside waitUntil"
+                            executeShellScript 'exit 1'       
+                        }
+                    }
+                }
+                echo "after catchError"
+                echo "please note that catchInterruptions allow you to procced in case of timeout"
+                echo "but build status became ABORTED and you are not able to change it back to SUCCESS"
             }
         }
     }
 }
+
 boolean executeShellScript(String script) {
+    echo "executeShellScript(String script: ${script})"
     int status = sh script: script, returnStatus: true
     return status == 0
+}
+
+def waitUntilSuccessfulResponse(String url) {
+    def response
+    boolean isResponseStatusSuccessful = false
+    long timeout = getUnixTime() + 5 * 1000
+    echo "before catchError, catchInterruptions: ${params.CATCH_INTERRUPTIONS}"
+    catchError(message: 'waitUntil gave up, but we continue...', catchInterruptions: params.CATCH_INTERRUPTIONS, buildResult: 'SUCCESS', stageResult: 'SUCCESS') {
+        waitUntil {
+            response = httpRequest httpMode: 'GET', url: url, validResponseCodes: '100:999'
+            echo "${url} has http status code ${response.status}"
+            isResponseStatusSuccessful = ( response.status >= 200 && response.status < 400 )
+            return isResponseStatusSuccessful || isTimeoutReached(timeout)
+        }
+    }
+    echo "after catchError"
+    return response
+}
+
+boolean isTimeoutReached(long timeout) {
+    long now = getUnixTime()
+    boolean isTimeoutReached = ( now > timeout )
+    echo "now: ${now}, timeout: ${timeout}, isTimeoutReached: ${isTimeoutReached}"
+    return isTimeoutReached
+}
+
+// In sandbox mode getting: Scripts not permitted to use method java.util.Date getTime. Administrators can decide whether to approve or reject this signature.
+long getUnixTime() { 
+    return new Date().getTime()
 }
